b5793f3b72fb9b14186c752ee3181285ec217f20
halvorin <halvorin@uio.no>
2023-04-13 00:16:24 +0200

    basic functionality for Oslo

--- index.js
+++ index.js
@@ -13,12 +13,63 @@ async function geoLocSuccess(position) {
       lon
   );
   const jsonData = await response.json();
-  console.log(jsonData);
-  console.log(jsonData.kommunenavn);
+  geoLocDone(jsonData.kommunenavn);
 }
 function geoLocError() {
   console.log("geoLocerror");
-  content.innerHTML = "could not get location";
+  alert("Vi fant ikke din posisjon, så vi antar at du er i Oslo");
+  geoLocDone("Oslo");
+}
+
+async function geoLocDone(kommuneNavn) {
+  var year = new Date().getFullYear();
+
+  const hoytiderResponse = await fetch(
+    "https://webapi.no/api/v1/holidays/" + year
+  );
+  const hoytiderJson = await hoytiderResponse.json();
+  var hoytider = hoytiderJson.data;
+  hoytider.shift();
+  console.log(hoytider);
+  var today = new Date().toISOString().slice(0, 10);
+  var tomorrow = new Date(Date.now() + 86400000);
+
+  tomorrow = tomorrow.toISOString().slice(0, 10);
+
+  const kommuner = await fetch("kommuner.json");
+  var kommuneData = await kommuner.json();
+
+  var kommuneData = kommuneData.filter(
+    (kommune) => kommune.kommuneNavn === kommuneNavn
+  )[0];
+  console.log(kommuneData);
+
+  hoytider.forEach((hoytid) => {
+    switch (hoytid.date.slice(0, 10)) {
+      case today:
+        document.getElementById(
+          "content"
+        ).innerHTML = `<h1>Det selges ikke alkohol i dag!</h1>`;
+        break;
+      case tomorrow:
+        var timeString = hoytid.description.replace("ø", "o");
+        timeString = timeString.description.replace("æ", "e");
+        timeString = timeString.description.replace("å", "a");
+        timeString = timeString.description.replace(" ", "");
+        timeString = "dayBefore" + timeString;
+        times = kommuneData.timeString.split("-");
+        document.getElementById(
+          "content"
+        ).innerHTML = `<h1>i dag selges det alkohol fra ${times[0]} til ${times[1]}</h1>`;
+        break;
+      default:
+        var times = kommuneData.default.split("-");
+        document.getElementById(
+          "content"
+        ).innerHTML = `<h1>Det selges alkohol fra ${times[0]} til ${times[1]}</h1>`;
+        break;
+    }
+  });
 }
 
 function main() {
