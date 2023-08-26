e2373b4ee612109dc442ceabc5ce1ea69c04d385
halvornj <halvornjaatad@gmail.com>
2023-08-26 21:29:00 +0200

    minor work on ReactApp

--- ReactApp/App.tsx
+++ ReactApp/App.tsx
@@ -2,18 +2,24 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Alert } from "react-native";
 import * as Location from "expo-location";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useState } from "react";
 
 import Button from "./components/Button";
-import ComingWeek from "./components/ComingWeek";
 import WeekView from "./components/WeekView";
 
 import Kommune from "./Kommune";
 
 //main renderer i guess
 export default function App() {
+  const [salesTimes, setSalesTimes] = useState<String>("loading");
+  const updateSalesTimes = (time: String) => {
+    setSalesTimes(time);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>fuck you</Text>
+      <Text style={styles.salesTimesFlavourText}>explanation goes here</Text>
+      <Text style={styles.salesTimes}>{salesTimes}</Text>
       <View style={styles.comingWeek}>
         <WeekView></WeekView>
       </View>
@@ -38,6 +44,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: 200,
     height: 200,
+  },
+  salesTimesFlavourText: {
+    fontSize: 20,
+  },
+  salesTimes: {
+    fontSize: 30,
   },
 });
 
@@ -77,6 +89,7 @@ export const getUserLocation = async () => {
 
 async function main() {
   const hasPermission = await checkPermission();
+
   var kommunenavn: String = "Oslo";
   if (hasPermission) {
     console.log("have permission, getting coords now");
@@ -117,18 +130,29 @@ async function geoLocDone(kommuneNavn: String) {
   //*join-point
   console.log("kommunenavn:");
   console.log(kommuneNavn);
-  const holidays = await getHolidays();
+  const holidays: JSON = await getHolidays();
 
   const userKommune: Kommune[] = await getMunicipality(kommuneNavn);
-  console.log(userKommune);
-  console.log(await getMunicipality());
+
+  const today: Date = new Date(Date.now());
+  const salesTimes: String = findSalesTimes(userKommune[0], holidays, today);
+  console.log(salesTimes);
+  if (
+    salesTimes === null ||
+    salesTimes === undefined ||
+    salesTimes === "stengt"
+  ) {
+    //?how the fuck do i update the state from outside the component??
+    App.updateSalesTimes("stengt");
+  } else {
+  }
 }
 
 /**
  *
  * @returns JSON-array of holidays for the current year
  */
-async function getHolidays(): Promise<Object[]> {
+async function getHolidays(): Promise<JSON> {
   const year = new Date().getFullYear();
   if ((await AsyncStorage.getItem(year.toString())) == null) {
     //there is no asyncStorage entry for current year
@@ -152,26 +176,96 @@ async function getHolidays(): Promise<Object[]> {
  * @returns an array with only the spesific Kommune-object if parameter was supplied, array of all Kommune-objects if no parameter was given.
  */
 const getMunicipality = async (name?: String): Promise<Kommune[]> => {
-  if ((await AsyncStorage.getItem("kommuner")) === null) {
-    //first ever fetch of the kommuner-list
-    const kommunerResponse = await fetch(
-      "https://raw.githubusercontent.com/halvornj/olsalget/main/kommuner.json"
-    );
-    const kommunerJSON = await kommunerResponse.json();
-    AsyncStorage.setItem("kommuner", JSON.stringify(kommunerJSON));
-    if (name != null) {
-      return kommunerJSON.find((kommune) => kommune.kommuneNavn === name);
-    } else {
-      return kommunerJSON;
-    }
+  const kommunerResponse = await fetch(
+    "https://raw.githubusercontent.com/halvornj/olsalget/main/kommuner.json"
+  );
+  const kommunerJSON = await kommunerResponse.json();
+  if (name != null) {
+    return [kommunerJSON.find((kommune) => kommune.kommuneNavn === name)];
   } else {
-    const kommunerJSON = JSON.parse(await AsyncStorage.getItem("kommuner"));
-    if (name != null) {
-      return kommunerJSON.find((kommune) => kommune.kommuneNavn === name);
-    } else {
-      return kommunerJSON;
+    return kommunerJSON;
+  }
+};
+
+/**
+ * @param kommune Kommune-object containing the hoytidsdata for a kommune.
+ * @param hoytider the full hoytids-data for the current year. can be retrieved with getHolidays().
+ * @param today the date for the spesific salestimes, as a Date-object.
+ * @returns string representing salestimes for today, e.g."09-20". if closed, returns null.
+ */
+const findSalesTimes = (kommune: Kommune, hoytider, today: Date) => {
+  const tomorrow: Date = new Date(today.getTime() + 86400000);
+  const tomorrowStr: String = tomorrow.toISOString().slice(0, 10);
+  const todayStr: String = today.toISOString().slice(0, 10);
+
+  const alkoholLoven = new Kommune(kommune.kommuneNavn, false);
+  if (!kommune.utvidet) {
+    kommune = alkoholLoven;
+  }
+
+  var hoytidISOStrings: String[] = [];
+  for (var i = 0; i < hoytider.length; i++) {
+    hoytidISOStrings.push(hoytider[i].date.slice(0, 10));
+  }
+
+  // *logikk
+  if (today.getDay() === 0) {
+    //today is sunday
+    return null;
+  }
+  if (hoytidISOStrings.includes(todayStr)) {
+    return null;
+  }
+
+  //fuck lindesnes, all my homies hate lindesnes, respectfully
+  if (kommune.kommuneNavn === "Lindesnes") {
+    for (i = 0; i < hoytider.length; i++) {
+      var hoytid = hoytider[i];
+      if (hoytid.date.slice(0, 10) === tomorrowStr) {
+        if (today.getDay() === 6) {
+          return kommune.sat;
+        } else {
+          return kommune.default;
+        }
+      }
     }
   }
+  //special jan 1. case
+  if (tomorrowStr.slice(5, 10) === "01-01") {
+    return kommune.Forstenyttarsdag;
+  }
+
+  //handling day before all holidays
+  for (i = 0; i < hoytider.length; i++) {
+    var hoytid = hoytider[i];
+
+    if (hoytid.date.slice(0, 10) === tomorrowStr) {
+      //tomorrow is a holiday
+      var hoyTidString = hoytid.description;
+      //this could absolutely be done w/ regex, but i'm too lazy to figure it out
+      hoyTidString = hoyTidString
+        .replace("æ", "e")
+        .replace("ø", "o")
+        .replace("å", "a")
+        .replace(" ", "");
+
+      if (kommune[hoyTidString] === "standard") {
+        if (today.getDay() === 6) {
+          return kommune.sat;
+        }
+        return kommune.default;
+      }
+      return kommune[hoyTidString];
+    }
+  }
+  if (todayStr.slice(5, 10) === "12-23" && kommune.Lillejulaften != undefined) {
+    return kommune.Lillejulaften;
+  }
+
+  if (today.getDay() === 6) {
+    return kommune.sat;
+  }
+  return kommune.default;
 };
 
 main();
