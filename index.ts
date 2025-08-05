054fcedb6fe10f7e96eda0258b23cd726dbf7fca
halvornj <halvor@njastad.no>
2025-08-05 22:25:29 +0200

    added dataclass in lib.ts, and started the basic stuff just to get something on paper

--- index.ts
+++ index.ts
@@ -1,1 +1,39 @@
-console.log("typerscrirprt");
+//for the first pass, I'm writing this in the same style i like to write C
+//imports
+import { Municipality } from "./lib";
+
+//defines/macros
+//#define TODAY_IDX 0;
+const TODAY_IDX: number = 0;
+
+/*global ui state variables
+ */
+let weekTimes: Array<string> = [
+  "Loading...",
+  "Loading...",
+  "Loading...",
+  "Loading...",
+  "Loading...",
+  "Loading...",
+  "Loading...",
+];
+let currentMunicipality: Municipality = new Municipality( //no named arguments?? really...
+  "ukjent", // kommunenavn
+  null, // altnavn
+  null, // electionday
+  "08-15", // forstejuledag
+  "08-15", // forstenyttarsdag
+  "08-15", // forstepinsedag
+  null, // grunnlovsdag
+  null, // kristihimmelfartsdag
+  null, // offentlighoytidsdag
+  "08-15", // skjertorsdag
+  "08-15", // forstepaskedag
+  "08-18", // standard
+  "08-15", // saturday
+  "08-15" // palmesondag
+);
+
+/*getters and setters? for ui states
+ */
+const getMainDisplay = () => weekTimes[0];
---
+++ lib.ts
@@ -0,0 +1,53 @@
+export class Municipality {
+  readonly kommunenavn: string;
+  readonly altnavn: string | null;
+  readonly electionday: string | null;
+  readonly forstejuledag: string | null;
+  readonly forstenyttarsdag: string | null;
+  readonly forstepinsedag: string | null;
+  readonly grunnlovsdag: string | null;
+  readonly kristihimmelfartsdag: string | null;
+  readonly offentlighoytidsdag: string | null;
+  readonly skjertorsdag: string | null;
+  readonly forstepaskedag: string | null;
+  readonly standard: string;
+  readonly saturday: string;
+  readonly palmesondag: string | null;
+
+  constructor(
+    kommunenavn: string,
+    altnavn: string | null,
+    electionday: string | null,
+    forstejuledag: string | null,
+    forstenyttarsdag: string | null,
+    forstepinsedag: string | null,
+    grunnlovsdag: string | null,
+    kristihimmelfartsdag: string | null,
+    offentlighoytidsdag: string | null,
+    skjertorsdag: string | null,
+    forstepaskedag: string | null,
+    standard: string,
+    saturday: string,
+    palmesondag: string | null
+  ) {
+    this.kommunenavn = kommunenavn;
+    this.altnavn = altnavn;
+    this.electionday = electionday;
+    this.forstejuledag = forstejuledag;
+    this.forstenyttarsdag = forstenyttarsdag;
+    this.forstepinsedag = forstepinsedag;
+    this.grunnlovsdag = grunnlovsdag;
+    this.kristihimmelfartsdag = kristihimmelfartsdag;
+    this.offentlighoytidsdag = offentlighoytidsdag;
+    this.skjertorsdag = skjertorsdag;
+    this.forstepaskedag = forstepaskedag;
+    this.standard = standard;
+    this.saturday = saturday;
+    this.palmesondag = palmesondag;
+  }
+
+  getStringForDate(date: Date): string {
+    //TODO
+    return "00-24";
+  }
+}
